e774ae7325c6fc38b787c79db181ade0b41d0f26
Boytik Evgeny <slyfox1994@yandex.ru>
2025-10-31 14:43:21 +0300

    Add files via upload

---
+++ pylt/TasteCore/WhabApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhabApp.swift
+//  Whab
+//
+//  Created by Евгений on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TasteCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
